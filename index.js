088cf9581f63c0e954909669ed90fa0a37393fab
Facundo Perez <perezfacundo11@hotmail.com>
2023-07-29 16:37:13 +0200

    se añadio un reloj

---
+++ index.js
@@ -0,0 +1,66 @@
+//let edadPersonal = 28;
+//let nombrePersonal = "Facundo";
+//let numeroUno = parseInt("5");
+//let numeroDos = 2;
+
+//let resultadoSuma = numeroDos + numeroUno;
+
+//console.log("resultado suma", resultadoSuma);
+
+// ejemplos 
+
+//let saludo = "hola, bienvenido";
+//let nombre = "Facundo";
+//let saludoCompleto = saludo + " " + nombre;
+
+//console.log("apa kaixo", saludoCompleto);
+
+//comando prompt + let para guardar la variable
+
+//let persona = prompt("ingrese su nombre");
+//alert(saludo + " " + persona);
+
+//console.log(typeof numero);
+//console.log(typeof nombre);
+//console.log(typeof numero);
+
+//parseInt(sirve con enteros y desimales)  - parseFloat - number (en consola?) se usan para convertir string en numero
+// ejemplo parseInt("5")
+
+
+
+
+
+//desafio
+
+let saludo = "hola, bienvenido";
+let persona = prompt("ingrese su nombre");
+alert(`${saludo} ${persona}`);
+
+let numeroUno = parseInt("5");
+let number = parseInt(prompt("ingrese un numero"));
+alert(number + numeroUno);
+
+let ciudad = prompt("ingrese una ciudad");
+let codigoPostal = prompt("ingrese un codigo postal");
+alert(ciudad + " " + codigoPostal + " " + "envios disponibles");
+
+let numeroif = parseInt(prompt("elija numero del 1 al 10"));
+if (numeroif < 6) { alert("5 o menor")}
+else if (numeroif < 11) { alert("5 o mayor") }
+else{ alert("respuesta incorrecta") };
+
+
+
+
+//adicionales
+//reloj
+
+
+
+
+
+
+
+/* ------------------FUNCIONANDO EN LA PAGINA--------------------- */
+
